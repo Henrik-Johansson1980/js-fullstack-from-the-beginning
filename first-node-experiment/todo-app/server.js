ae324a0a4507f12c18456b8e859999c02e4f7c31
Henrik Johansson <henrikjohansson1980@gmail.com>
2020-05-04 12:26:42 +0200

    Simple-Todo-App complete

--- first-node-experiment/todo-app/server.js
+++ first-node-experiment/todo-app/server.js
@@ -1,83 +1,100 @@
 let express = require('express')
 let mongodb = require('mongodb')
+let sanitizeHTML = require('sanitize-html')
 let app = express()
 let db 
 
-let connetctionString = 'mongodb+srv://todoAppUser:<password>@cluster0-dapll.mongodb.net/TodoApp?retryWrites=true&w=majority'
+app.use(express.static('public'))
+
+let connetctionString = 'mongodb+srv://todoAppUser:LDLZIEuMd16lmMrw@cluster0-dapll.mongodb.net/TodoApp?retryWrites=true&w=majority'
 
 mongodb.connect( 
   connetctionString,
-  {useNewUrlParser: true},
+  {useNewUrlParser: true, useUnifiedTopology: true},
   (err, client) => {
     db = client.db()
     app.listen(3000)
   }
 )
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res)=>{
+function  passwordProtected (req, res, next) {
+  res.set('WWW-Authenticate', 'Basic realm="Simple Todo App"')
+  if(req.headers.authorization == "Basic bGVhcm5pbmc6amF2YXNjcmlwdF9wcm9ncmFtbWluZw=="){
+    next()
+  } else {
+    res.status(401).send("Authentication Required")
+  }
+}
+
+//Use passwordprotection for all urls
+app.use(passwordProtected)
+
+app.get('/' , (req, res)=>{
+  //Read from db and convert to array
+  db.collection('items').find().toArray((err, items)=>{
+     //Send response
   res.send(`
-    <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="display-4 text-center py-1">To-Do App</h1>
-      
-      <div class="jumbotron p-3 shadow-sm">
-        <form action="/create-item" method="POST">
-          <div class="d-flex align-items-center">
-            <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-            <button class="btn btn-primary">Add New Item</button>
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple To-Do App</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link rel="shortcut icon" href="#" />
+      </head>
+      <body>
+        <div class="container">
+          <h1 class="display-4 text-center py-1">To-Do App</h1>
+          
+          <div class="jumbotron p-3 shadow-sm">
+            <form id="create-form" action="/create-item" method="POST">
+              <div class="d-flex align-items-center">
+                <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                <button class="btn btn-primary">Add New Item</button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      
-      <ul class="list-group pb-5">
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #1</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #2</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #3</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-      </ul>
-      
-    </div>
-    
-  </body>
-  </html>
-  `)
+          
+          <ul id="item-list" class="list-group pb-5">
+          </ul>
+
+        </div>
+        <script>
+          let items = ${JSON.stringify(items)}
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <script src="/browser.js"></script>
+      </body>
+      </html>
+    `)
+  })
 })
 
 app.post('/create-item', (req, res) => {
-    db.collection('items').insertOne({text: req.body.item}, () => {
-
-    res.send('Tnx form stuff')
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {} })
+    db.collection('items').insertOne({text: safeText }, (err, info) => {
+    res.json(info.ops[0])
   })
-
-  console.log(req.body.item)
-
 })
 
+app.post('/update-item', (req, res) => {
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {} })
+  db.collection('items').findOneAndUpdate(
+    { _id: new mongodb.ObjectID( req.body.id ) }, 
+    { $set: {text: safeText} },
+    () => {
+      res.send("Success")
+    }
+  )}
+)
+
+app.post('/delete-item', (req,res) => {
+  db.collection('items').deleteOne({_id: new mongodb.ObjectID(req.body.id)}, ()=>{
+    res.send('Success')
+  })
+})
 
